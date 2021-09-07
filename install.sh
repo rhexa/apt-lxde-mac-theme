9ccbc595fc157c5aaa3d6fd7cbddcfcaa81cc6d1
rhexa <akun.kripitit@gmail.com>
2021-09-07 22:15:01 +0300

    updated install script

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cd ~/.themes
 
 # OpenBox theme
 # SBColors_XOS.obt https://www.box-look.org/p/1336941/
-wget --content-disposition https://github.com/rhexa/apt-lxde-mac-theme/raw/main/themes/SBColors_XOS.obt
+wget -O SBColors_XOS.obt https://github.com/rhexa/apt-lxde-mac-theme/raw/main/openbox/SBColors_XOS.obt
 tar -xvf SBColors_XOS.obt
 rm SBColors_XOS.obt
 # sed -ri 's#(<titleLayout>).*(</titleLayout>)#\1CIMLN\2#g' .config/openbox/lxde-rc.xml
@@ -24,13 +24,13 @@ rm mcOS-BS-White-Stock.zip
 # McMuse.tar.xz https://store.kde.org/p/1348081
 mkdir ~/.icons
 cd ~/.icons
-wget --content-disposition https://github.com/rhexa/apt-lxde-mac-theme/raw/main/icons/McMuse.tar.xz
+wget -O McMuse.tar.xz https://github.com/rhexa/apt-lxde-mac-theme/raw/main/icons/McMuse.tar.xz
 tar -xvf McMuse.tar.xz
 rm McMuse.tar.xz
 
 # Cursor
 # McMojave-cursors.tar.xz https://store.kde.org/p/1355701/
-wget --content-disposition https://github.com/rhexa/apt-lxde-mac-theme/raw/main/icons/cursors/McMojave-cursors.tar.xz
+wget -O McMojave-cursors.tar.xz https://github.com/rhexa/apt-lxde-mac-theme/raw/main/icons/cursors/McMojave-cursors.tar.xz
 tar -xvf McMojave-cursors.tar.xz
 rm McMojave-cursors.tar.xz
 
